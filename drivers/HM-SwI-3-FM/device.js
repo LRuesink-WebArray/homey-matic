1e175c735c6b83f97ccdca52d5c124c5f8ca4c41
Timo Wendt <timo@tjwendt.de>
2018-09-02 00:39:23 +0200

    Add INSTALL_TEST triggers for HM-SwI-3-FM

--- drivers/HM-SwI-3-FM/device.js
+++ drivers/HM-SwI-3-FM/device.js
@@ -21,6 +21,17 @@ class HomematicDevice extends Device {
         Homey.app.hm.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':3-PRESS', (value) => {
             self._driver.triggerButtonPressedFlow3(self, {}, 3)
         });
+
+        Homey.app.hm.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':1-INSTALL_TEST', (value) => {
+            console.log("Button 1 pressed");
+            self._driver.triggerButtonPressedFlow1(self, {}, 1)
+        });
+        Homey.app.hm.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':2-INSTALL_TEST', (value) => {
+            self._driver.triggerButtonPressedFlow2(self, {}, 2)
+        });
+        Homey.app.hm.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':3-INSTALL_TEST', (value) => {
+            self._driver.triggerButtonPressedFlow3(self, {}, 3)
+        });
     }
 }
 
