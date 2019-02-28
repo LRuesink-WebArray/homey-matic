914160e39f58a4c191cdada95fb6f55d1d580d57
gooooba <dd@vklop.com>
2019-02-28 10:48:07 +0100

    Update device.js

--- drivers/HMIP-PSM/device.js
+++ drivers/HMIP-PSM/device.js
@@ -16,6 +16,16 @@ const capabilityMap = {
         "channel": 6,
         "key": "POWER"
     },
+    "measure_voltage": {
+        "channel": 6,
+        "key": "VOLTAGE",
+        "valueType": "V"
+    },
+    "measure_current": {
+        "channel": 6,
+        "key": "CURRENT",
+        "valueType": "V"
+    },
     "meter_power": {
         "channel": 6,
         "key": "ENERGY_COUNTER",
