408a86152d8de312c989405f2778814ddb25c0a7
Bjoern <bwelker@gmail.com>
2019-04-02 14:09:19 +0200

    Update device.js

--- drivers/HM-Sec-Key/device.js
+++ drivers/HM-Sec-Key/device.js
@@ -24,7 +24,7 @@ const capabilityMap = {
         "key": "OPEN",
         "valueType": "keymatic",
         "set": {
-            "key": "STATE",
+            "key": "OPEN",
             "channel": 1,
             "valueType": "keymatic"
         }
