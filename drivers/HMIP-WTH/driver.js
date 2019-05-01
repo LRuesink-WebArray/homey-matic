588eac2be9f3223112301c07fee5469301c59491
Timo Wendt <timo@tjwendt.de>
2019-05-01 22:00:01 +0200

    Add HmIP-WTH-2

--- drivers/HMIP-WTH/driver.js
+++ drivers/HMIP-WTH/driver.js
@@ -15,7 +15,7 @@ class HomematicDriver extends Driver {
             'homematic_thermostat_boost',
             'homematic_thermostat_weekprofile'
         ]
-        this.homematicTypes = ['HMIP-WTH'];
+        this.homematicTypes = ['HMIP-WTH', 'HmIP-WTH-2'];
         this.log(this.homematicTypes.join(','), 'has been inited');
     }
 
