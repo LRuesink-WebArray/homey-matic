6f8c0cc0b5dc7a72a8b54fa56800a3ad89ecc8c2
Timo Wendt <timo@tjwendt.de>
2019-05-26 18:32:28 +0200

    Add HM-Sen-DB-PCB

---
+++ drivers/HM-Sen-DB-PCB/device.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Homey = require('homey');
+const Device = require('../../lib/device.js')
+
+const capabilityMap = {
+    "alarm_battery": {
+        "channel": 0,
+        "key": "LOWBAT",
+        "valueType": "boolean"
+    }
+}
+
+class HomematicDevice extends Device {
+
+    onInit() {
+        super.onInit(capabilityMap);
+        this._driver = this.getDriver();
+    }
+
+    initializeExtraEventListeners() {
+        var self = this;
+        self.bridge.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':1-PRESS_SHORT', (value) => {
+            self._driver.triggerButtonPressedFlow(self, { "button": 1 }, { "button": 1, "pressType": "short" })
+        });
+    }
+}
+
+module.exports = HomematicDevice;
