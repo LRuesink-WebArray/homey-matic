8b149824918f51ee80af0f1b51cf7fd573936b94
Timo Wendt <timo@tjwendt.de>
2019-06-02 21:46:05 +0200

    Revert settings page changes

--- app.js
+++ app.js
@@ -2,37 +2,22 @@
 
 const Homey = require('homey');
 const HomeMaticDiscovery = require('./lib/HomeMaticDiscovery');
-const HomeMaticCCU = require('./lib/HomeMaticCCU');
+const inspector = require('inspector');
 class Homematic extends Homey.App {
 
   onInit() {
+    // inspector.open(9229, '0.0.0.0');
     var self = this;
     this.log('Started homematic...');
     Homey.ManagerCloud.getLocalAddress()
       .then((address) => {
         self.homeyIP = address.split(':')[0]
       })
-      .then(function () {
-        self.bridges = {};
 
-        self.settings = self.getSettings();
-        if (self.settings.ccu_host != "") {
-          self.bridges["static"] = new HomeMaticCCU("CCU", "static", self.settings.ccu_host)
-          return
-        }
-        self.discovery = new HomeMaticDiscovery();
-      })
-
-    // this.discovery.discover()
-
-    // this.bridges = {};
+    self.discovery = new HomeMaticDiscovery();
+    this.bridges = {};
   }
 
-  getSettings() {
-    return {
-      "ccu_host": Homey.ManagerSettings.get('ccu_host'),
-    }
-  }
 }
 
 
