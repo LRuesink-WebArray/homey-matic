cb131c18e841b7ffcbf4f676657e62cc50b183f6
Timo Wendt <timo@tjwendt.de>
2019-06-20 15:59:51 +0200

    Add HM-LC-Bl1-FM

---
+++ drivers/HM-LC-Bl1-FM/driver.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Homey = require('homey');
+const Driver = require('../../lib/driver.js');
+
+class HomematicDriver extends Driver {
+
+    onInit() {
+        super.onInit();
+        this.capabilities = [
+            "windowcoverings_state",
+            "windowcoverings_set"
+        ];
+        this.homematicTypes = ['HM-LC-Bl1-FM'];
+        this.log(this.homematicTypes.join(','), 'has been inited');
+    }
+}
+
+module.exports = HomematicDriver;
