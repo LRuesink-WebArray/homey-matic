8e3297c3e260b929bfc87a635ec7ecc2de4a7192
Timo Wendt <timo@tjwendt.de>
2019-06-20 15:41:41 +0200

    Add HM-LC-Sw4-DR

---
+++ drivers/HM-LC-Sw4-DR/driver.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Homey = require('homey');
+const Driver = require('../../lib/driver.js');
+
+class HomematicDriver extends Driver {
+
+    onInit() {
+        super.onInit();
+        this.capabilities = [
+            'onoff'
+        ]
+        this.homematicTypes = ['HM-LC-Sw4-DR']
+        this.numDevices = 4
+        this.multiDevice = true
+        this.log(this.homematicTypes.join(','), 'has been inited');
+    }
+
+
+}
+
+module.exports = HomematicDriver;
