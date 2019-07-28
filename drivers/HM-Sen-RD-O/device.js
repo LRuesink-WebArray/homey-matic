7703d5fe5abc0c5f3a5946ba71bb0a850430b913
Timo Wendt <timo@tjwendt.de>
2019-07-28 18:41:36 +0200

    Add HM-Sen-RD-O

---
+++ drivers/HM-Sen-RD-O/device.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Homey = require('homey');
+const Device = require('../../lib/device.js')
+
+const capabilityMap = {
+    "homematic_alarm_rain": {
+        "channel": 1,
+        "key": "STATE",
+        "convert": Convert.toBoolean
+    },
+    "onoff": {
+        "channel": 2,
+        "key": "STATE",
+        "set": {
+            "key": "STATE",
+            "channel": 2
+        }
+    }
+}
+
+class HomematicDevice extends Device {
+
+    onInit() {
+        super.onInit(capabilityMap);
+        this._driver = this.getDriver();
+    }
+
+    initializeExtraEventListeners() {
+        var self = this;
+        let tokens = {};
+        let state = {};
+        self.bridge.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':1-STATE', (value) => {
+            if (value == 1) {
+                self._driver.triggerTurnedOn(self, tokens, state)
+            } else {
+                self._driver.triggerTurnedOff(self, tokens, state)
+            }
+        });
+
+    }
+}
+
+module.exports = HomematicDevice;
