ff99d7b9989ec6646e0598c8b522a68749ad3e0d
Timo Wendt <timo@tjwendt.de>
2019-07-28 15:48:59 +0200

    Add HM-MOD-Re-8

---
+++ drivers/HM-MOD-Re-8/driver.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Homey = require('homey');
+const Driver = require('../../lib/driver.js');
+
+class HomematicDriver extends Driver {
+    onInit() {
+        super.onInit();
+        this.capabilities = [
+            'onoff'
+        ]
+        this.homematicTypes = ['HM-MOD-Re-8'];
+        this.numDevices = 8
+        this.multiDevice = true
+        this.log(this.homematicTypes.join(','), 'has been inited');
+    }
+
+}
+
+module.exports = HomematicDriver;
