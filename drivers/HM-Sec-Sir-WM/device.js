6659717faf62fac2cf667ebb5bdd34ce294fc2ad
Timo Wendt <timo@tjwendt.de>
2019-10-18 15:11:34 +0200

    Add HM-Sec-Sir-WM

---
+++ drivers/HM-Sec-Sir-WM/device.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Homey = require('homey');
+const Device = require('../../lib/device.js')
+
+class HomematicDevice extends Device {
+
+    onInit() {
+        var idx = this.getData().attributes.Index;
+        var capabilityMap = {
+            "onoff": {
+                "channel": idx + 1,
+                "key": "STATE",
+                "set": {
+                    "key": "STATE",
+                    "channel": idx + 1
+                }
+            },
+            "alarm_battery": {
+                "channel": 1,
+                "key": "LOWBAT",
+                "convert": "boolean"
+            }
+        }
+        super.onInit(capabilityMap);
+    }
+}
+
+module.exports = HomematicDevice;
