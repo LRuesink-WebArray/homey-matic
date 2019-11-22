4ccb3570001da96e5b2a0e025a3ee06dfdc53fae
Timo Wendt <timo@tjwendt.de>
2019-11-22 22:06:39 +0100

    Add HM-Sec-SC

---
+++ drivers/HM-Sec-SC/driver.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Homey = require('homey');
+const Driver = require('../../lib/driver.js');
+
+class HomematicDriver extends Driver {
+
+    onInit() {
+        super.onInit();
+        this.capabilities = [
+            'alarm_contact', 'alarm_battery'
+        ]
+        this.homematicTypes = ['HM-Sec-SC']
+        this.log(this.homematicTypes.join(','), 'has been inited');
+    }
+
+
+}
+
+module.exports = HomematicDriver;
