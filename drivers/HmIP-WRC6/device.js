5657696d8382906a5a583ad0c1e283aa57f6ab4a
Timo Wendt <timo@tjwendt.de>
2020-04-20 21:01:47 +0200

    Add battery alarm to HmIP-WRC6

--- drivers/HmIP-WRC6/device.js
+++ drivers/HmIP-WRC6/device.js
@@ -2,8 +2,15 @@
 
 const Homey = require('homey');
 const Device = require('../../lib/device.js')
+const Convert = require('../../lib/convert.js')
 
-const capabilityMap = {}
+const capabilityMap = {
+    "alarm_battery": {
+        "channel": 0,
+        "key": "LOW_BAT",
+        "convert": Convert.toBoolean
+    }
+}
 
 class HomematicDevice extends Device {
 
@@ -15,10 +22,10 @@ class HomematicDevice extends Device {
         var self = this;
         for (let button = 1; button <= 6; button++) {
             self.bridge.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':' + button + '-PRESS_SHORT', (value) => {
-                self.driver.triggerButtonPressedFlow(self, { "button": button }, { "button": button, "pressType": "short" })
+                self.driver.triggerButtonPressedFlow(self, {"button": button}, {"button": button, "pressType": "short"})
             });
             self.bridge.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':' + button + '-PRESS_LONG', (value) => {
-                self.driver.triggerButtonPressedFlow(self, { "button": button }, { "button": button, "pressType": "long" })
+                self.driver.triggerButtonPressedFlow(self, {"button": button}, {"button": button, "pressType": "long"})
             });
         }
 
