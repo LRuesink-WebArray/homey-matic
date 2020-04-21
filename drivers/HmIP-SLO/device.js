3ccfa2f6a17631ed2779c4de037dfd3aa4e2b080
Timo Wendt <timo@tjwendt.de>
2020-04-21 21:12:10 +0200

    Use sub categories in HmIP-SLO

--- drivers/HmIP-SLO/device.js
+++ drivers/HmIP-SLO/device.js
@@ -4,9 +4,24 @@ const Homey = require('homey');
 const Device = require('../../lib/device.js')
 
 const capabilityMap = {
-    "measure_luminance": {
+    "measure_luminance.current": {
         "channel": 1,
         "key": "CURRENT_ILLUMINATION",
+        "valueType": "float"
+    },
+    "measure_luminance.average": {
+        "channel": 1,
+        "key": "AVERAGE_ILLUMINATION",
+        "valueType": "float"
+    },
+    "measure_luminance.highest": {
+        "channel": 1,
+        "key": "HIGHEST_ILLUMINATION",
+        "valueType": "float"
+    },
+    "measure_luminance.lowest": {
+        "channel": 1,
+        "key": "LOWEST_ILLUMINATION",
         "valueType": "float"
     },
     "alarm_battery": {
@@ -22,6 +37,23 @@ class HomematicDevice extends Device {
         super.onInit(capabilityMap);
     }
 
+    initializeExtraEventListeners() {
+        var self = this;
+        self.bridge.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':1-CURRENT_ILLUMINATION', (value) => {
+            self.driver.triggerCurrentIlluminanceChangedFlow(self, {"Illuminance": self.getCapabilityValue('measure_luminance.current')}, {})
+        });
+        self.bridge.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':1-AVERAGE_ILLUMINATION', (value) => {
+            self.driver.triggerAverageIlluminanceChangedFlow(self, {"Illuminance": self.getCapabilityValue('measure_luminance.average')}, {})
+        });
+        self.bridge.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':1-HIGHEST_ILLUMINATION', (value) => {
+            self.driver.triggerHighestIlluminanceChangedFlow(self, {"Illuminance": self.getCapabilityValue('measure_luminance.highest')}, {})
+        });
+        self.bridge.on('event-' + self.HomeyInterfaceName + '-' + self.deviceAddress + ':1-LOWEST_ILLUMINATION', (value) => {
+            self.driver.triggerLowestIlluminanceChangedFlow(self, {"Illuminance": self.getCapabilityValue('measure_luminance.lowest')}, {})
+        });
+
+
+    }
 }
 
 module.exports = HomematicDevice;
