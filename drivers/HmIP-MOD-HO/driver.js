88b51adaeb52b1ec9fadeb80ec9ebbc8d2617d5e
Timo Wendt <timo@tjwendt.de>
2020-11-15 19:29:39 +0100

    Add HmIP-MOD-HO

---
+++ drivers/HmIP-MOD-HO/driver.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Homey = require('homey');
+const Driver = require('../../lib/driver.js');
+
+class HomematicDriver extends Driver {
+
+    onInit() {
+        super.onInit();
+        this.capabilities = [
+            "measure_mod_ho_door_state",
+            "windowcoverings_set",
+            "homematic_button_mod_ho_partial_open"
+        ];
+        this.homematicTypes = ['HmIP-MOD-HO'];
+        this.log(this.homematicTypes.join(','), 'has been inited');
+
+        let partialOpenAction = new Homey.FlowCardAction('homematic_mod_ho_set_partial_open');
+        weekprofileAction
+            .register()
+            .registerRunListener((args, state) => {
+                return args.device.triggerCapabilityListener('homematic_button_mod_ho_partial_open', true, {})
+            })
+    }
+}
+
+module.exports = HomematicDriver;
