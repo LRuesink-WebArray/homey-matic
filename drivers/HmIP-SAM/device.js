d0fb1a03277ebda864418854e96cc874949e48ee
Timo Wendt <timo@tjwendt.de>
2021-02-08 21:29:25 +0100

    Debug logging HmIP-SAM

--- drivers/HmIP-SAM/device.js
+++ drivers/HmIP-SAM/device.js
@@ -4,11 +4,19 @@ const Homey = require('homey');
 const Device = require('../../lib/device')
 const Convert = require('../../lib/convert')
 
+const toBoolean = function (value) {
+    console.log("HmIP-SAM value:", JSON.stringify(value))
+    if (value === 0) {
+        return false
+    }
+    return true
+}
+
 const capabilityMap = {
     "alarm_motion": {
         "channel": 1,
         "key": "MOTION",
-        "convert": Convert.toBoolean
+        "convert": toBoolean
     },
     "alarm_battery": {
         "channel": 0,
