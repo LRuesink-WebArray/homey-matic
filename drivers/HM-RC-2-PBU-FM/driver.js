cd2ee2d01ed45528168c8dd7e9bd4ee42e10d82b
afalter <48165619+afalter@users.noreply.github.com>
2022-02-10 13:16:27 +0100

    Added new driver for HM-RC-2-PBU-FM

---
+++ drivers/HM-RC-2-PBU-FM/driver.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Homey = require('homey');
+const Driver = require('../../lib/driver.js');
+
+class HomematicDriver extends Driver {
+
+    onInit() {
+        super.onInit();
+        this.capabilities = []
+        this.homematicTypes = ['HM-RC-2-PBU-FM'];
+        this.log(this.homematicTypes.join(','), 'has been inited');
+
+        this._flowTriggerButtonPressed = this.homey.flow.getDeviceTriggerCard('HM-RC-2-PBU-FM-press')
+            .registerRunListener((args, state) => {
+                if (args.button == state.button && args.pressType == state.pressType) {
+                    return Promise.resolve(true)
+                } else {
+                    return Promise.reject(false)
+                }
+            })
+    }
+
+    triggerButtonPressedFlow(device, tokens, state) {
+        this._flowTriggerButtonPressed
+            .trigger(device, tokens, state)
+            .catch(this.error)
+    }
+}
+
+module.exports = HomematicDriver;
