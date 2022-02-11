e7a16bb12dca80bad04e0527870f5a64ed03ef28
twendt <timo@tjwendt.de>
2022-02-11 23:57:23 +0100

    Add HmIP-FCI1 (#128)

---
+++ drivers/HmIP-FCI1/device.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Homey = require('homey');
+const Device = require('../../lib/device.js');
+
+const capabilityMap = {}
+
+class HomematicDevice extends Device {
+
+    onInit() {
+        var idx = this.getData().attributes.Index;
+        super.onInit(capabilityMap);
+    }
+
+    initializeExtraEventListeners() {
+        var self = this;
+        for (let button = 1; button <= 1; button++) {
+            self.bridge.on('event-' + self.deviceAddress + ':' + button + '-PRESS_SHORT', (value) => {
+                self.driver.triggerButtonPressedFlow(self, { "button": button }, { "button": button, "pressType": "short" })
+            });
+            self.bridge.on('event-' + self.deviceAddress + ':' + button + '-PRESS_LONG', (value) => {
+                self.driver.triggerButtonPressedFlow(self, { "button": button }, { "button": button, "pressType": "long" })
+            });
+        }
+    }
+
+}
+
+module.exports = HomematicDevice;
